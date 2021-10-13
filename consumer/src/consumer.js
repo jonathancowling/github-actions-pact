38a71cafde8111438dbca44244a935c3bfef02b8
Jonathan Cowling <jonathan.cowling@santander.co.uk>
2021-10-13 10:45:12 +0100

    Added env from provider

--- consumer/src/consumer.js
+++ consumer/src/consumer.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const fetch = require("node-fetch").default;
 
 module.exports = async function request() {
-  const res = await fetch(`${process.emv.HOST}${process.env.ENDPOINT_PATH}`)
+  const res = await fetch(`${process.env.HOST}${process.env.ENDPOINT_PATH}`)
   if (!res.ok) {
     throw new Error("api error");
   }
